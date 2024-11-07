1da9c5e27c932972e8329b33a74851b31088237a
filipkk78 <filipkk78@gmail.com>
2024-11-07 20:26:56 +0100

    Implement task adding ui

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import "./styles.css";
 const myTasks = [];
 
 class task {
-    constructor(title, description, dueDate, priority, project) {
+    constructor(title, description, dueDate, priority, projectId) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
-        this.project = `${myProjects.length+1}-${project}`;
-        myProjects.at(this.project.at(1)).push(this);
+        myProjects.at(projectId).tasks.push(this);
     }
 }
 
@@ -27,10 +26,21 @@ class project {
 new project("Default");
 new project("Work");
 new project("Home");
+new task("get 1500 vbucks", "secure the funds for perfect cell when he arrives in the fortnite store", "09-11-2024", "Max", "2")
+console.log(myProjects);
 
-const projectDisplay = document.querySelector(".projects")
+const projectList = document.querySelector("#project")
 for(let i = 0; i < myProjects.length; i++) {
-    const project = document.createElement("li");
+    const project = document.createElement("option");
     project.textContent = `${myProjects.at(i).title}`;
-    projectDisplay.appendChild(project);
+    project.setAttribute("value", i);
+    projectList.appendChild(project);
 }
+
+document.querySelector("#newtask").addEventListener("click", () => {
+    document.querySelector(".popup").style.display = "flex";
+})
+
+document.querySelector("#addtaskbtn").addEventListener("click", () => {
+    document.querySelector(".popup").style.display = "none";
+})
