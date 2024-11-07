1d66b9c1e633b4d25d6b98bef643bb31fe34d356
filipkk78 <filipkk78@gmail.com>
2024-11-07 19:23:10 +0100

    Implement creating and displaying projects

--- src/index.js
+++ src/index.js
@@ -1,2 +1,36 @@
 import "./styles.css";
-console.log("Test");
+
+const myTasks = [];
+
+class task {
+    constructor(title, description, dueDate, priority, project) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.project = `${myProjects.length+1}-${project}`;
+        myProjects.at(this.project.at(1)).push(this);
+    }
+}
+
+const myProjects = [];
+
+class project {
+    constructor(title) {
+        this.id = `${myProjects.length+1}`;
+        this.title = `${title}`;
+        this.tasks = [];
+        myProjects.push(this);
+    }
+}
+
+new project("Default");
+new project("Work");
+new project("Home");
+
+const projectDisplay = document.querySelector(".projects")
+for(let i = 0; i < myProjects.length; i++) {
+    const project = document.createElement("li");
+    project.textContent = `${myProjects.at(i).title}`;
+    projectDisplay.appendChild(project);
+}
