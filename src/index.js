77ba156af8853a0bfe6a565f02c3ae76df68802d
filipkk78 <filipkk78@gmail.com>
2024-11-12 19:01:12 +0100

    Change the way of displaying tasks

--- src/index.js
+++ src/index.js
@@ -22,21 +22,95 @@ class project {
         this.tasks = [];
         myProjects.push(this);
     }
+    displayTasks() {
+        const mainBlock = document.querySelector(".main-block");
+        function updateTaskCount(title, taskCount) {
+            currentProject.textContent = `${title} (${taskCount})`;
+        }
+    
+        const currentProject = document.createElement("h2");
+        currentProject.classList.add("current-project");
+        updateTaskCount(this.title, this.tasks.length);
+        mainBlock.appendChild(currentProject);
+    
+        const tasks = document.createElement("div");
+        tasks.classList.add("tasks");
+        mainBlock.appendChild(tasks);
+
+        let projectName = this.title;
+        let taskList = this.tasks;
+
+        taskList.forEach(task => {
+            const taskCard = document.createElement("div");
+            taskCard.classList.add("task-card");
+            
+            const taskCardHeader = document.createElement("div");
+            taskCardHeader.classList.add("task-card-header");
+            
+            const taskTitle = document.createElement("h4");
+            taskTitle.textContent = task.title;
+            taskCardHeader.appendChild(taskTitle);
+
+            const deleteButton = document.createElement("div");
+            deleteButton.classList.add("delete-button");
+            deleteButton.addEventListener("click", () => {
+                tasks.removeChild(taskCard);
+                taskList.splice(taskList.indexOf(task), 1);
+                console.clear();
+                updateTaskCount(projectName, taskList.length);
+            })
+
+            taskCardHeader.appendChild(taskTitle);
+            taskCardHeader.appendChild(deleteButton);
+
+            taskCard.appendChild(taskCardHeader);
+
+            const taskDesc = document.createElement("div");
+            taskDesc.classList.add("task-description");
+            taskDesc.textContent = task.description;
+            taskCard.appendChild(taskDesc);
+
+            const taskCardFooter = document.createElement("div");
+            taskCardFooter.classList.add("task-card-footer");
+
+            const taskPriority = document.createElement("h4");
+            const taskPriorityLevel = document.createElement("span");
+            taskPriorityLevel.classList.add(`priority-${task.priority}`);
+            taskPriorityLevel.textContent = task.priority;
+            taskPriority.textContent = "Priority: ";
+            taskPriority.appendChild(taskPriorityLevel);
+            taskCardFooter.appendChild(taskPriority);
+
+            const taskDueDate = document.createElement("h4");
+            taskDueDate.textContent = `Due: ${task.dueDate}`;
+            taskCardFooter.appendChild(taskDueDate);   
+        
+            taskCard.appendChild(taskCardFooter);
+
+            tasks.appendChild(taskCard);
+    });
+    }
+}
+
+function clearPage() {
+    const main = document.querySelector(".main-block");
+    while(main.lastElementChild) {
+        main.removeChild(main.lastElementChild);
+    }
 }
 
 new project("Default");
 new project("Work");
 new project("Home");
-// new task("put the mango down", "this is for my winter arc", "09-11-2024", "Medium", "2");
-// new task("buy stillwater", "those who know", "11-12-2025", "High", "1");
-// new task("put the fries in the bag", "just put the fries in the bag bro", "11-11-2024", "Low", "0");
-
-// for (let j = 0; j < 3; j++) {
-//     for (let i = 0; i < 5; i++) {
-//         new task(`task ${i+1}`, `this is the description of task number ${i+1}`, "20-12-2025", "Low", j);
-//     }
-// }
-
+new task("put the mango down", "this is for my winter arc", "09-11-2024", "Medium", "0");
+new task("buy stillwater", "those who know", "11-12-2025", "High", "0");
+new task("put the fries in the bag", "just put the fries in the bag bro", "11-11-2024", "Low", "0");
+new task("Feed the dog", "Give the dog some food", "11-11-2024", "Medium", "2");
+new task("Feed the cat", "Give the cat some food", "11-11-2024", "High", "2");
+new task("Clean the house", "Vaccum and wipe the floors", "14-11-2024", "Low", "2");
+new task("Prepare lunch", "Make some food to eat at work", "12-11-2024", "High", "1");
+new task("Finish the app", "Complete the app you were working on recently", "26-11-2024", "High", "1");
+new task("Buy new clothes", "Get some new clothe for work", "15-11-2024", "Low", "1");
 
 const projectList = document.querySelector("#project")
 for(let i = 0; i < myProjects.length; i++) {
@@ -52,7 +126,7 @@ document.querySelector("#newtask").addEventListener("click", () => {
     popUp.style.display = "flex";
 })
 
-const form = document.querySelector("form");
+const form = document.querySelector("#taskform");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     const fd = new FormData(form);
@@ -63,13 +137,13 @@ form.addEventListener("submit", (e) => {
     loadHomePage();
 })
 
-
 document.querySelector(".header-title").addEventListener("click", () => {
-    console.log(myProjects);
-})
-
-document.querySelector("#resetform").addEventListener("click", () => {
-    form.reset();
+    console.clear();
+    myProjects.forEach(project => {
+        project.tasks.forEach(task => {
+            console.log(task);
+        })
+    })
 })
 
 document.querySelector("#exitform").addEventListener("click", () => {
@@ -77,5 +151,30 @@ document.querySelector("#exitform").addEventListener("click", () => {
     form.reset();
 })
 
-export default myProjects;
-loadHomePage();
+const projectPopUp = document.querySelector(".project-popup"); 
+
+document.querySelector("#projects").addEventListener("click", () => {
+    projectPopUp.style.display = "flex";
+})
+
+document.querySelector("#exitprojectform").addEventListener("click", () => {
+    projectPopUp.style.display = "none";
+    form.reset();
+})
+
+const pForm = document.querySelector("#project-form");
+pForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const fd = new FormData(pForm);
+    const obj = Object.fromEntries(fd);
+    console.log(obj);
+    new project(obj.projectname);
+    projectPopUp.style.display = "none";
+    pForm.reset();
+    loadHomePage();
+})
+
+export {myProjects, clearPage}
+// loadHomePage();
+
+document.querySelector("#home").addEventListener("click", loadHomePage);
--- src/modules/home.js
+++ src/modules/home.js
@@ -1,83 +1,9 @@
-import myProjects from "..";
+import {myProjects, clearPage} from "..";
 
 function loadHomePage() {
-    const mainBlock = document.querySelector(".main-block");
-    while (mainBlock.firstChild) {
-        mainBlock.removeChild(mainBlock.lastChild);
-    }
-
-    function updateTaskCount() {
-        let taskCount = 0;
-        myProjects.forEach(project => {
-            taskCount+=project.tasks.length;
-        });
-        currentProject.textContent = `All (${taskCount})`;
-    }
-
-    const currentProject = document.createElement("h2");
-    currentProject.classList.add("current-project");
-    updateTaskCount();
-    mainBlock.appendChild(currentProject);
-
-    const tasks = document.createElement("div");
-    tasks.classList.add("tasks");
-    mainBlock.appendChild(tasks);
-    
+    clearPage();
     myProjects.forEach(project => {
-        project.tasks.forEach(task => {
-                const taskCard = document.createElement("div");
-                taskCard.classList.add("task-card");
-                
-                const taskCardHeader = document.createElement("div");
-                taskCardHeader.classList.add("task-card-header");
-                
-                const taskTitle = document.createElement("h4");
-                taskTitle.textContent = task.title;
-                taskCardHeader.appendChild(taskTitle);
-    
-                const deleteButton = document.createElement("div");
-                deleteButton.classList.add("delete-button");
-                deleteButton.addEventListener("click", () => {
-                    tasks.removeChild(taskCard);
-                    project.tasks.splice(project.tasks.indexOf(task), 1);
-                    console.clear();
-                    myProjects.forEach(project => {
-                        project.tasks.forEach(task => {
-                            console.log(task);
-                        })
-                    })
-                    updateTaskCount();
-                })
-
-                taskCardHeader.appendChild(taskTitle);
-                taskCardHeader.appendChild(deleteButton);
-    
-                taskCard.appendChild(taskCardHeader);
-    
-                const taskDesc = document.createElement("div");
-                taskDesc.classList.add("task-description");
-                taskDesc.textContent = task.description;
-                taskCard.appendChild(taskDesc);
-
-                const taskCardFooter = document.createElement("div");
-                taskCardFooter.classList.add("task-card-footer");
-
-                const taskPriority = document.createElement("h4");
-                const taskPriorityLevel = document.createElement("span");
-                taskPriorityLevel.classList.add(`priority-${task.priority}`);
-                taskPriorityLevel.textContent = task.priority;
-                taskPriority.textContent = "Priority: ";
-                taskPriority.appendChild(taskPriorityLevel);
-                taskCardFooter.appendChild(taskPriority);
-
-                const taskDueDate = document.createElement("h4");
-                taskDueDate.textContent = `Due: ${task.dueDate}`;
-                taskCardFooter.appendChild(taskDueDate);   
-            
-                taskCard.appendChild(taskCardFooter);
-
-                tasks.appendChild(taskCard);
-        });
+        project.displayTasks();
     })
 };
 
